60e16d4faabae768a0be7ce7a8e76205f09442a8
GainfulAlloy <126686936+GainfulAlloy@users.noreply.github.com>
2023-03-06 18:22:05 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,10 +8,6 @@
             // creates a new Pack of cards
             // shuffles the cards based on users input
             // deals a specified number of cards
-
-
-
-
             Testing newTest = new Testing();
             newTest.testing();
         }   
